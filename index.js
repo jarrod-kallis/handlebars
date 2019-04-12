a5cba51f0085e45647668ac0a8f7882889b734a5
Jarrod Kallis <jarrod.kallis@gmail.com>
2019-04-12 11:36:35 +0200

    Some logging

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 (function() {
   $.get(
     'http://localhost:8080/templates/title.handlebars',
-    function(data) {
-      console.log(data);
-      var template = Handlebars.compile(data);
-      $('#root').html(
-        template({
-          title: 'Jarrod was here',
-          author: 'Jarrod Kallis',
-          reviewer: 'Tess Kallis'
-        })
-      );
+    function(htmlData) {
+      const data = {
+        title: 'Jarrod was here',
+        author: 'Jarrod Kallis',
+        reviewer: 'Tess Kallis'
+      };
+      console.log('htmlData', htmlData);
+      console.log('data', data);
+
+      var template = Handlebars.compile(htmlData);
+      console.log('template', template(data));
+
+      $('#root').html(template(data));
     },
     'html' // not really required it would seem
   );
